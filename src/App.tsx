92c63f0891e909a10cb4a88a6e9fe8be5a3bf463
Trung Quoc Nguyen <johnnguyen2202@outlook.com>
2020-12-05 14:07:23 +0700

    Install React Navigation packages

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return <></>;
+};
+
+export default App;
